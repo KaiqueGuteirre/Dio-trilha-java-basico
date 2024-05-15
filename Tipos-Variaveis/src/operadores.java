7319b5627c7ae58eef4449342efe9e4e7efc6ca7
KaiqueGutierre <kaiquegutierre@gmail.com>
2024-05-15 17:46:46 -0300

    projeto smart TV

--- Tipos-Variaveis/src/operadores.java
+++ Tipos-Variaveis/src/operadores.java
@@ -3,7 +3,7 @@ public class operadores {
     public static void main(String[] args) {
         String nomeCompleto = "LINGUAGEM" + "JAVA";
         int numero = 5;
-        numero = numero + 2;
+        numero = numero++;
         System.out.println(numero);
 
     }
