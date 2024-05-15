0074a1e50e4e5f2ca60508540fc97f1ac63d8119
KaiqueGutierre <kaiquegutierre@gmail.com>
2024-05-15 16:19:22 -0300

    inicio

---
+++ Tipos-Variaveis/src/operadores.java
@@ -0,0 +1,11 @@
+public class operadores {
+
+    public static void main(String[] args) {
+        String nomeCompleto = "LINGUAGEM" + "JAVA";
+        int numero = 5;
+        numero = numero + 2;
+        System.out.println(numero);
+
+    }
+
+}
